ace9a7d4fd1697a42083479430ff7519ea041897
endemic <ndemick@gmail.com>
2011-01-04 22:38:48 +0000

    Updated title screen background

--- Classes/TitleScene.mm
+++ Classes/TitleScene.mm
@@ -30,42 +30,36 @@
 		
 		CGSize windowSize = [CCDirector sharedDirector].winSize;
 		
-		// Add moving background
-		CCSprite *background = [CCSprite spriteWithFile:@"title-screen-background.png"];
+		// Add background
+		NSMutableString *backgroundFile = [NSMutableString stringWithFormat:@"title-background"];
+		
+		// Check if running on iPad
+		if ([GameData sharedGameData].isTablet) [backgroundFile appendString:@"-hd"];
+		
+		// Append file extension
+		[backgroundFile appendString:@".png"];
+		
+		CCSprite *background = [CCSprite spriteWithFile:backgroundFile];
 		[background setPosition:ccp(windowSize.width / 2, windowSize.height / 2)];
 		[self addChild:background z:0];
-		
-		// Add game logo
-		CCSprite *logo = [CCSprite spriteWithFile:@"logo.png"];
-		[logo setPosition:ccp(160, 370)];
-		[self addChild:logo z:1];
 		
 		// Add button which takes us to game scene
 		CCMenuItem *startButton = [CCMenuItemImage itemFromNormalImage:@"start-button.png" selectedImage:@"start-button.png" target:self selector:@selector(startGame:)];
 		CCMenu *titleMenu = [CCMenu menuWithItems:startButton, nil];
-		[titleMenu setPosition:ccp(160, 50)];
+		[titleMenu setPosition:ccp(windowSize.width / 2, windowSize.height / 10)];
 		[self addChild:titleMenu z:1];
 		
 		// Check if running on iPad
 		if ([GameData sharedGameData].isTablet)
-		{
-			[background setScale:2.0];
-			[background setPosition:ccp(windowSize.width / 2, windowSize.height / 2)];
-			
-			[logo setScale:2.0];
-			[logo setPosition:ccp(320, 740)];
-			
 			[startButton setScale:2.0];
-			[startButton setPosition:ccp(320, 100)];
-		}
 		
 		// Run animation which moves background
-		[background runAction:[CCRepeatForever actionWithAction:[CCSequence actions:
-																 [CCDelayTime actionWithDuration:1.0],
-																 [CCMoveTo actionWithDuration:15.0 position:ccp(0, 240)], 
-																 [CCDelayTime actionWithDuration:1.0],
-																 [CCMoveTo actionWithDuration:15.0 position:ccp(320, 240)], 
-																 nil]]];
+//		[background runAction:[CCRepeatForever actionWithAction:[CCSequence actions:
+//																 [CCDelayTime actionWithDuration:1.0],
+//																 [CCMoveTo actionWithDuration:15.0 position:ccp(0, 240)], 
+//																 [CCDelayTime actionWithDuration:1.0],
+//																 [CCMoveTo actionWithDuration:15.0 position:ccp(320, 240)], 
+//																 nil]]];
 	}
 	return self;
 }
