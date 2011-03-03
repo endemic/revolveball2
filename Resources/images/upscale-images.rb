7bdb873e8c377548c8dd0fa01cae27704083c72a
endemic <ndemick@gmail.com>
2011-03-03 11:38:04 +0000

    Housekeeping w/ images

---
+++ Resources/images/upscale-images.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# Get all images in current directory + subdirectories
+maps = Dir['**/*.png']
+
+maps.each { |filename|
+  # Only process "sd" images
+  next if filename.index('-hd')
+
+  # Determine base filename/extension
+  filename_base, filename_extension = filename.split('.')
+  
+  # Run ImageMagick shell command to increase size 2x w/ nearest neighbor filter, then re-save w/ -hd suffix
+  # `convert #{filename} -resize 200% -filter Point #{filename_base}-hd.#{filename_extension}`
+  puts "convert #{filename} -resize 200% -filter Point #{filename_base}-hd.#{filename_extension}"
+}
