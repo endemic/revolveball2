cb31366c10f7f602e23d2a9df77995fd815bb740
endemic <ndemick@gmail.com>
2011-03-31 10:32:19 +0000

    Map tweaking

---
+++ Classes/CreditsScene.h
@@ -0,0 +1,16 @@
+//
+//  CreditsScene.h
+//  RevolveBall
+//
+//  Created by Nathan Demick on 3/31/11.
+//  Copyright 2011 Ganbaru Games. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface CreditsScene : CCLayer {
+
+}
+
+@end
---
+++ Classes/CreditsScene.m
@@ -0,0 +1,26 @@
+//
+//  CreditsScene.m
+//  RevolveBall
+//
+//  Created by Nathan Demick on 3/31/11.
+//  Copyright 2011 Ganbaru Games. All rights reserved.
+//
+
+#import "CreditsScene.h"
+
+
+@implementation CreditsScene
+- (id)init
+{
+	if ((self = [super init]))
+	{
+		// < Back
+		// Thanks for playing!
+		// You are a Revolve Ball Master!
+		// Try to get even faster times on your favorite levels.
+		// Created by Nathan Demick
+		// (more games)
+	}
+	return self;
+}
+@end
